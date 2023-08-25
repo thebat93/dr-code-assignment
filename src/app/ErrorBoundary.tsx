53c2cada59133019544cdc2185daba608f8c9f56
Igor Alekseev <ais_93@mail.ru>
2023-08-25 11:58:00 +0400

    Error Boundary component

---
+++ src/app/ErrorBoundary.tsx
@@ -0,0 +1,31 @@
+import { ReactNode, Component } from "react";
+
+interface Props {
+  children: ReactNode;
+  fallback: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
+
+export { ErrorBoundary };
