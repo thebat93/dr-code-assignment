191f2a12774a50b85f56ebf4e37ac39894720a90
Igor Alekseev <ais_93@mail.ru>
2023-08-25 11:56:20 +0400

    Next.js config for Image component

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: process.env.CDN_HOST,
+        port: "",
+        pathname: process.env.IMAGES_PATH,
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
