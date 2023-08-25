acaec97e08c6b22323495327bd6983f88ba574bc
Igor Alekseev <ais_93@mail.ru>
2023-08-25 11:57:49 +0400

    Page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,87 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
 
-export default function Home() {
+import { Suspense, useCallback } from "react";
+import clsx from "clsx";
+import { useSuspenseQuery } from "@apollo/experimental-nextjs-app-support/ssr";
+import { Variables } from "@/models/Variables";
+import { RecipesList } from "@/models/RecipesList";
+import { SearchField } from "@/components/SearchField";
+import { RecipeList } from "@/components/RecipeList";
+import { useDebouncedSearch } from "@/hooks/useDebouncedSearch";
+import { useLoadMoreRecipes } from "@/hooks/useLoadMoreRecipes";
+import { GET_RECIPES } from "./getRecipesQuery";
+import { ErrorBoundary } from "./ErrorBoundary";
+import s from "./page.module.css";
+
+export const dynamic = "force-dynamic";
+
+const Error = () => (
+  <div className={clsx(s.center, s.error)}>Something went wrong</div>
+);
+
+const initialQuery: Variables = {
+  search: undefined,
+  pageSize: 5,
+  page: 1,
+};
+
+const Page = () => {
+  const { data, error, fetchMore, refetch } = useSuspenseQuery<
+    RecipesList,
+    Variables
+  >(GET_RECIPES, {
+    variables: initialQuery,
+  });
+
+  const search = useCallback(
+    (newSearch: string) => {
+      if (newSearch.length > 0) {
+        refetch({ search: newSearch });
+      }
+
+      refetch(initialQuery);
+    },
+    [refetch]
+  );
+
+  const onSearchDebounced = useDebouncedSearch(search);
+
+  const { onLoadMore, isPending: isLoadMorePending } = useLoadMoreRecipes({
+    nextPage: data?.listRecipes.nextPage ?? null,
+    fetchMore,
+  });
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <>
+      <header className={s.header}>
+        <SearchField
+          name="search"
+          label="Recipes"
+          placeholder="Search your favourite recipes"
+          defaultQuery={initialQuery.search}
+          onSearch={onSearchDebounced}
+          autoFocus={true}
         />
-      </div>
+      </header>
+      <main className={s.main}>
+        <div className={s.content}>
+          <ErrorBoundary fallback={<Error />}>
+            <Suspense fallback={<p className={s.center}>Loading recipes...</p>}>
+              {error ? (
+                <Error />
+              ) : (
+                <RecipeList
+                  isLoading={isLoadMorePending}
+                  recipes={data.listRecipes.recipes}
+                  onLoadMore={onLoadMore}
+                />
+              )}
+            </Suspense>
+          </ErrorBoundary>
+        </div>
+      </main>
+    </>
+  );
+};
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Page;
